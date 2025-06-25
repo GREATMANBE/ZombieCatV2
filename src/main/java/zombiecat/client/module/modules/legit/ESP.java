4b7f20477cafe48a878275a0e1d7cd74a291ec78
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-06-25 09:50:19 +0100

    Update ESP.java

--- src/main/java/zombiecat/client/module/modules/legit/ESP.java
+++ src/main/java/zombiecat/client/module/modules/legit/ESP.java
@@ -15,8 +15,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
 import zombiecat.client.module.Module;
 import zombiecat.client.utils.Utils;
-import net.minecraft.potion.PotionEffect;
-
 
 import java.awt.*;
 import java.util.List;
@@ -52,74 +50,63 @@ public class ESP extends Module {
       }
    }
 
-public void trace() {
-    Entity thePlayer = mc.thePlayer;
-    if (thePlayer == null) return;
+   public void trace() {
+      Entity thePlayer = mc.thePlayer;
+      if (thePlayer == null) return;
 
-    GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-    GL11.glEnable(GL11.GL_BLEND);
-    GL11.glEnable(GL11.GL_LINE_SMOOTH);
-    GL11.glLineWidth(2);
-    GL11.glDisable(GL11.GL_TEXTURE_2D);
-    GL11.glDisable(GL11.GL_DEPTH_TEST);
-    GL11.glDepthMask(false);
+      GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+      GL11.glEnable(GL11.GL_BLEND);
+      GL11.glEnable(GL11.GL_LINE_SMOOTH);
+      GL11.glLineWidth(2);
+      GL11.glDisable(GL11.GL_TEXTURE_2D);
+      GL11.glDisable(GL11.GL_DEPTH_TEST);
+      GL11.glDepthMask(false);
 
-    GL11.glBegin(GL11.GL_LINES);
+      GL11.glBegin(GL11.GL_LINES);
 
-    List<Entity> entities = mc.theWorld.loadedEntityList;
-    Color color = new Color(255, 0, 0, 150);
-
-    for (Entity entity : entities) {
-        if (entity != thePlayer) {
-            if (entity instanceof EntityZombie
-                    && ((EntityZombie) entity).isChild()
-                    && entity.getInventory() != null
-                    && entity.getInventory()[0] != null
-                    && entity.getInventory()[0].getItem() == Items.diamond_sword) {
-                drawTraces(entity, color);
+      List<Entity> entities = mc.theWorld.loadedEntityList;
+      Color color = new Color(255, 0, 0, 150);
+      for (Entity entity : entities) {
+         if (entity != thePlayer) {
+            if (entity instanceof EntityZombie && ((EntityZombie) entity).isChild() && entity.getInventory() != null && entity.getInventory()[0] != null && entity.getInventory()[0].getItem() == Items.diamond_sword) {
+               drawTraces(entity, color);
+            if (entity instanceof EntityZombie && entity.getName().equals("King Slime") && ((EntityLivingBase) entity).isPotionActive(Potion.damageBoost) && ((EntityLivingBase) entity).getTotalArmorValue() >= 8) {
+               drawTraces(entity, color);
             }
+         }
+      }
 
-            if (entity instanceof EntityLivingBase
-                    && entity.getName().contains("King Slime")) {
-                EntityLivingBase living = (EntityLivingBase) entity;
-                if (living.getMaxHealth() >= 400.0F || living.getTotalArmorValue() >= 8) {
-                    drawTraces(entity, new Color(250, 0, 0, 150));
-                }
-            }
-        }
-    }
+      GL11.glEnd();
 
-    GL11.glEnd();
+      GL11.glEnable(GL11.GL_TEXTURE_2D);
+      GL11.glDisable(GL11.GL_LINE_SMOOTH);
+      GL11.glEnable(GL11.GL_DEPTH_TEST);
+      GL11.glDepthMask(true);
+      GL11.glDisable(GL11.GL_BLEND);
+      GL11.glColor4f(1f, 1f, 1f, 1f);
+   }
+   
+   private void drawTraces(Entity entity, Color color) {
+      Entity thePlayer = mc.thePlayer;
+      if (thePlayer == null) return;
 
-    GL11.glEnable(GL11.GL_TEXTURE_2D);
-    GL11.glDisable(GL11.GL_LINE_SMOOTH);
-    GL11.glEnable(GL11.GL_DEPTH_TEST);
-    GL11.glDepthMask(true);
-    GL11.glDisable(GL11.GL_BLEND);
-    GL11.glColor4f(1f, 1f, 1f, 1f);
-} // <-- close trace() here
+      double x = (entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * Utils.Client.getTimer().renderPartialTicks
+              - mc.getRenderManager().viewerPosX);
+      double y = (entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * Utils.Client.getTimer().renderPartialTicks
+              - mc.getRenderManager().viewerPosY);
+      double z = (entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * Utils.Client.getTimer().renderPartialTicks
+              - mc.getRenderManager().viewerPosZ);
 
-private void drawTraces(Entity entity, Color color) {
-    Entity thePlayer = mc.thePlayer;
-    if (thePlayer == null) return;
+      float yaw = thePlayer.prevRotationYaw + (thePlayer.rotationYaw - thePlayer.prevRotationYaw) * Utils.Client.getTimer().renderPartialTicks;
+      float pitch = thePlayer.prevRotationPitch + (thePlayer.rotationPitch - thePlayer.prevRotationPitch) * Utils.Client.getTimer().renderPartialTicks;
 
-    double x = (entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * Utils.Client.getTimer().renderPartialTicks
-            - mc.getRenderManager().viewerPosX);
-    double y = (entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * Utils.Client.getTimer().renderPartialTicks
-            - mc.getRenderManager().viewerPosY);
-    double z = (entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * Utils.Client.getTimer().renderPartialTicks
-            - mc.getRenderManager().viewerPosZ);
+      Vec3 eyeVector = new Vec3(0.0, 0.0, 1.0).rotatePitch(-(float) Math.toRadians(pitch)).rotateYaw(-(float) Math.toRadians(yaw));
 
-    float yaw = thePlayer.prevRotationYaw + (thePlayer.rotationYaw - thePlayer.prevRotationYaw) * Utils.Client.getTimer().renderPartialTicks;
-    float pitch = thePlayer.prevRotationPitch + (thePlayer.rotationPitch - thePlayer.prevRotationPitch) * Utils.Client.getTimer().renderPartialTicks;
+      GL11.glColor4f(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
 
-    Vec3 eyeVector = new Vec3(0.0, 0.0, 1.0).rotatePitch(-(float) Math.toRadians(pitch)).rotateYaw(-(float) Math.toRadians(yaw));
-
-    GL11.glColor4f(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
-
-    GL11.glVertex3d(eyeVector.xCoord, thePlayer.getEyeHeight() + eyeVector.yCoord, eyeVector.zCoord);
-    GL11.glVertex3d(x, y, z);
-    GL11.glVertex3d(x, y, z);
-    GL11.glVertex3d(x, y + entity.height, z);
+      GL11.glVertex3d(eyeVector.xCoord, thePlayer.getEyeHeight() + eyeVector.yCoord, eyeVector.zCoord);
+      GL11.glVertex3d(x, y, z);
+      GL11.glVertex3d(x, y, z);
+      GL11.glVertex3d(x, y + entity.height, z);
+   }
 }
-
