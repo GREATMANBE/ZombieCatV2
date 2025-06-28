57b402bdde9660a55d142624f178b0e7225a3bf8
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-06-28 08:06:27 +0100

    Update StringSetting.java

--- src/main/java/zombiecat/client/module/setting/impl/StringSetting.java
+++ src/main/java/zombiecat/client/module/setting/impl/StringSetting.java
@@ -52,8 +52,37 @@ public class StringSetting extends Setting {
 
     @Override
     public Component createComponent(ModuleComponent parent) {
-        // You’ll want to implement your GUI component creation here
-        // Return a new Component that allows selecting or typing a string value
-        return null; // Placeholder: implement your own GUI component
+        return new Component(parent, this) {
+            private int currentIndex = getIndex();
+    
+            private int getIndex() {
+                for (int i = 0; i < options.length; i++) {
+                    if (options[i].equalsIgnoreCase(value)) return i;
+                }
+                return 0;
+            }
+    
+            @Override
+            public void drawComponent(int mouseX, int mouseY) {
+                // Basic drawing: label and current value
+                mc.fontRendererObj.drawStringWithShadow(
+                    getName() + ": " + value, 
+                    x + 2, y + 2, 
+                    0xFFFFFF
+                );
+            }
+    
+            @Override
+            public void mouseClicked(int mouseX, int mouseY, int button) {
+                if (isHovered(mouseX, mouseY) && button == 0) {
+                    currentIndex = (currentIndex + 1) % options.length;
+                    setValue(options[currentIndex]);
+                }
+            }
+    
+            private boolean isHovered(int mouseX, int mouseY) {
+                return mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height;
+            }
+        };
     }
 }
