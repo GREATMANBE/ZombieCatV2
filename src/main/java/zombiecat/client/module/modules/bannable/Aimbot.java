645c1411048ebba377fcc3e10c42eed29b2676a6
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-06-28 15:38:19 +0100

    Update Aimbot.java

--- src/main/java/zombiecat/client/module/modules/bannable/Aimbot.java
+++ src/main/java/zombiecat/client/module/modules/bannable/Aimbot.java
@@ -23,214 +23,150 @@ import zombiecat.client.module.setting.impl.SliderSetting;
 import zombiecat.client.utils.Utils;
 
 public class Aimbot extends Module {
-   public static BooleanSetting onlyFire;
-   public static BooleanSetting wsStair;
-   public static BooleanSetting pup; // Added pup toggle
-   public static SliderSetting a;
-   public static SliderSetting predict;
-   public static SliderSetting yPredict;
+    public static BooleanSetting onlyFire;
+    public static BooleanSetting wsStair;
+    public static BooleanSetting pup; // Added pup toggle
+    public static SliderSetting a;
+    public static SliderSetting predict;
+    public static SliderSetting yPredict;
+    public static SliderSetting aimYOffset; // NEW slider for Y aim offset
 
-   public Aimbot() {
-      super("Aimbot", ModuleCategory.bannable);
-      this.registerSetting(a = new SliderSetting("Fineness", 0.4, 0.1, 1.0, 0.1));
-      this.registerSetting(onlyFire = new BooleanSetting("OnlyFire", true));
-      this.registerSetting(wsStair = new BooleanSetting("WSStair", true));
-      this.registerSetting(pup = new BooleanSetting("Pup", false)); // default off
-      this.registerSetting(predict = new SliderSetting("Predict", 4, 0, 10, 0.1));
-      this.registerSetting(yPredict = new SliderSetting("YPredict", 4, 0, 10, 0.1));
-   }
+    public Aimbot() {
+        super("Aimbot", ModuleCategory.bannable);
+        this.registerSetting(a = new SliderSetting("Fineness", 0.4, 0.1, 1.0, 0.1));
+        this.registerSetting(onlyFire = new BooleanSetting("OnlyFire", true));
+        this.registerSetting(wsStair = new BooleanSetting("WSStair", true));
+        this.registerSetting(pup = new BooleanSetting("Pup", false)); // default off
+        this.registerSetting(predict = new SliderSetting("Predict", 4, 0, 10, 0.1));
+        this.registerSetting(yPredict = new SliderSetting("YPredict", 4, 0, 10, 0.1));
+        this.registerSetting(aimYOffset = new SliderSetting("Aim Y Offset", 1.5, 0.0, 3.0, 0.1)); // default 1.5 blocks
+    }
 
-   @SubscribeEvent
-   public void re(RenderWorldLastEvent e) {
-      if (onlyFire.getValue() && !mc.gameSettings.keyBindUseItem.isKeyDown()) {
-         return;
-      }
+    @SubscribeEvent
+    public void re(RenderWorldLastEvent e) {
+        if (onlyFire.getValue() && !mc.gameSettings.keyBindUseItem.isKeyDown()) {
+            return;
+        }
 
-      double dis = 9999999;
-      Vec3 target= null;
-      if (Utils.Player.isPlayerInGame()) {
-         for (Entity entity : mc.theWorld.loadedEntityList) {
-            if (entity instanceof EntityLivingBase
-                    && !(entity instanceof EntityArmorStand)
-                    && !(entity instanceof EntityWither)
-                    && !(entity instanceof EntityVillager)
-                    && !(entity instanceof EntityPlayer)
-                    && !(entity instanceof EntityChicken)
-                    && !(entity instanceof EntityPig)
-                    && !(entity instanceof EntityCow)
-                    && entity.isEntityAlive()) {
+        double dis = Double.MAX_VALUE;
+        Vec3 target = null;
+        if (Utils.Player.isPlayerInGame()) {
+            for (Entity entity : mc.theWorld.loadedEntityList) {
+                if (entity instanceof EntityLivingBase
+                        && !(entity instanceof EntityArmorStand)
+                        && !(entity instanceof EntityWither)
+                        && !(entity instanceof EntityVillager)
+                        && !(entity instanceof EntityPlayer)
+                        && !(entity instanceof EntityChicken)
+                        && !(entity instanceof EntityPig)
+                        && !(entity instanceof EntityCow)
+                        && entity.isEntityAlive()) {
 
-               // Puppy logic: skip puppy wolves if pup toggle is off
-               if (entity instanceof EntityWolf) {
-                  EntityWolf wolf = (EntityWolf) entity;
-                  if (!pup.getValue() && wolf.isChild()) {
-                     continue;
-                  }
-               }
-
-               Vec3 offset = getMotionVec(entity, (float) predict.getValue(), (float) yPredict.getValue());
-               double distance = fovDistance(entity.getPositionEyes(1).add(offset));
-               if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionEyes(1).add(offset))) {
-                  dis = distance;
-                  target = entity.getPositionEyes(1).add(offset);
-               } else {
-
-                  double yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                  distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.1, 0)));
-                  if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                     dis = distance;
-                     target = entity.getPositionVector().add(offset);
-                  } else {
-                     yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                     distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.2, 0)));
-                     if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                        dis = distance;
-                        target = entity.getPositionVector().add(offset);
-                     } else {
-                        yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                        distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.3, 0)));
-                        if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                           dis = distance;
-                           target = entity.getPositionVector().add(offset);
-                        } else {
-                           yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                           distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.4, 0)));
-                           if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                              dis = distance;
-                              target = entity.getPositionVector().add(offset);
-                           } else {
-                              yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                              distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.5, 0)));
-                              if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                 dis = distance;
-                                 target = entity.getPositionVector().add(offset);
-                              } else {
-                                 yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                                 distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.6, 0)));
-                                 if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                    dis = distance;
-                                    target = entity.getPositionVector().add(offset);
-                                 } else {
-                                    yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                                    distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.7, 0)));
-                                    if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                       dis = distance;
-                                       target = entity.getPositionVector().add(offset);
-                                    } else {
-                                       yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                                       distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.8, 0)));
-                                       if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                          dis = distance;
-                                          target = entity.getPositionVector().add(offset);
-                                       } else {
-                                          yOffset = entity.getPositionEyes(1).yCoord - entity.getPositionVector().yCoord;
-                                          distance = fovDistance(entity.getPositionVector().add(offset).add(new Vec3(0, -yOffset * 0.9, 0)));
-                                          if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                             dis = distance;
-                                             target = entity.getPositionVector().add(offset);
-                                          } else {
-                                             distance = fovDistance(entity.getPositionVector().add(offset));
-                                             if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), entity.getPositionVector().add(offset))) {
-                                                dis = distance;
-                                                target = entity.getPositionVector().add(offset);
-                                             }
-                                          }
-                                       }
-                                    }
-                                 }
-                              }
-                           }
+                    // Puppy logic: skip puppy wolves if pup toggle is off
+                    if (entity instanceof EntityWolf) {
+                        EntityWolf wolf = (EntityWolf) entity;
+                        if (!pup.getValue() && wolf.isChild()) {
+                            continue;
                         }
-                     }
-                  }
-               }
+                    }
+
+                    Vec3 offset = getMotionVec(entity, (float) predict.getValue(), (float) yPredict.getValue());
+
+                    // Calculate target position using adjustable Y offset
+                    Vec3 basePos = entity.getPositionVector().add(offset);
+                    Vec3 aimPos = new Vec3(basePos.xCoord, entity.getPositionVector().yCoord + aimYOffset.getValue(), basePos.zCoord);
+
+                    double distance = fovDistance(aimPos);
+                    if (distance < dis && canWallShot(mc.thePlayer.getPositionEyes(1), aimPos)) {
+                        dis = distance;
+                        target = aimPos;
+                    }
+                }
             }
-         }
-         if (target != null) {
-            float[] angle = calculateYawPitch(mc.thePlayer.getPositionVector().addVector(0,mc.thePlayer.getEyeHeight(),0), target);
-            mc.thePlayer.rotationYaw = angle[0];
-            mc.thePlayer.rotationPitch = angle[1];
-         }
-      }
-   }
+            if (target != null) {
+                float[] angle = calculateYawPitch(mc.thePlayer.getPositionVector().addVector(0, mc.thePlayer.getEyeHeight(), 0), target);
+                mc.thePlayer.rotationYaw = angle[0];
+                mc.thePlayer.rotationPitch = angle[1];
+            }
+        }
+    }
 
-   public static double fovDistance(Vec3 vec3) {
-      float[] angle = calculateYawPitch(mc.thePlayer.getPositionVector().addVector(0,mc.thePlayer.getEyeHeight(),0), vec3);
-      return angleBetween(angle[0], mc.thePlayer.rotationYaw) +
-              Math.abs(Math.max(angle[1], mc.thePlayer.rotationPitch) - Math.min(angle[1], mc.thePlayer.rotationPitch));
-   }
+    public static double fovDistance(Vec3 vec3) {
+        float[] angle = calculateYawPitch(mc.thePlayer.getPositionVector().addVector(0, mc.thePlayer.getEyeHeight(), 0), vec3);
+        return angleBetween(angle[0], mc.thePlayer.rotationYaw) +
+                Math.abs(Math.max(angle[1], mc.thePlayer.rotationPitch) - Math.min(angle[1], mc.thePlayer.rotationPitch));
+    }
 
-   public static double angleBetween(double first, double second) {
-      return Math.abs(subtractAngles(first, second));
-   }
+    public static double angleBetween(double first, double second) {
+        return Math.abs(subtractAngles(first, second));
+    }
 
-   public static double subtractAngles(double start, double end) {
-      return MathHelper.wrapAngleTo180_double(end - start);
-   }
+    public static double subtractAngles(double start, double end) {
+        return MathHelper.wrapAngleTo180_double(end - start);
+    }
 
-   public static boolean canWallShot(Vec3 start, Vec3 end) {
-      float[] angle = calculateYawPitch(start, end);
+    public static boolean canWallShot(Vec3 start, Vec3 end) {
+        float[] angle = calculateYawPitch(start, end);
 
-      Vec3 temp = fromPolar(angle[1], angle[0]);
-      Vec3 forward = new Vec3(temp.xCoord * a.getValue(), temp.yCoord * a.getValue(), temp.zCoord * a.getValue());
-      Vec3 now = start;
-      while (now.distanceTo(end) > a.getValue() + 0.1) {
-         Block block = mc.theWorld.getBlockState(new BlockPos(now)).getBlock();
-         if (block == Blocks.sandstone_stairs) {
-            return false;
-         }
-         if (block instanceof BlockSlab && ((BlockSlab) block).isDouble()) {
-            return false;
-         }
-         if (block instanceof BlockSlab || wsStair.getValue() && block instanceof BlockStairs && block != Blocks.spruce_stairs || block == Blocks.iron_door || block == Blocks.iron_bars || block instanceof BlockSign || block instanceof BlockBarrier) {
-            return true;
-         }
-         if (block != Blocks.air && block != Blocks.grass && block != Blocks.tallgrass) {
-            return false;
-         }
-         now = now.add(forward);
-      }
-      Block endBlock = mc.theWorld.getBlockState(new BlockPos(end)).getBlock();
-      return endBlock == Blocks.air || endBlock == Blocks.iron_bars || endBlock instanceof BlockSlab || endBlock instanceof BlockSign || endBlock instanceof BlockBarrier;
-   }
+        Vec3 temp = fromPolar(angle[1], angle[0]);
+        Vec3 forward = new Vec3(temp.xCoord * a.getValue(), temp.yCoord * a.getValue(), temp.zCoord * a.getValue());
+        Vec3 now = start;
+        while (now.distanceTo(end) > a.getValue() + 0.1) {
+            Block block = mc.theWorld.getBlockState(new BlockPos(now)).getBlock();
+            if (block == Blocks.sandstone_stairs) {
+                return false;
+            }
+            if (block instanceof BlockSlab && ((BlockSlab) block).isDouble()) {
+                return false;
+            }
+            if (block instanceof BlockSlab || (wsStair.getValue() && block instanceof BlockStairs && block != Blocks.spruce_stairs) || block == Blocks.iron_door || block == Blocks.iron_bars || block instanceof BlockSign || block instanceof BlockBarrier) {
+                return true;
+            }
+            if (block != Blocks.air && block != Blocks.grass && block != Blocks.tallgrass) {
+                return false;
+            }
+            now = now.add(forward);
+        }
+        Block endBlock = mc.theWorld.getBlockState(new BlockPos(end)).getBlock();
+        return endBlock == Blocks.air || endBlock == Blocks.iron_bars || endBlock instanceof BlockSlab || endBlock instanceof BlockSign || endBlock instanceof BlockBarrier;
+    }
 
-   public static Vec3 fromPolar(float pitch, float yaw) {
-      float f = MathHelper.cos(-yaw * 0.017453292F - 3.1415927F);
-      float g = MathHelper.sin(-yaw * 0.017453292F - 3.1415927F);
-      float h = -MathHelper.cos(-pitch * 0.017453292F);
-      float i = MathHelper.sin(-pitch * 0.017453292F);
-      return new Vec3(g * h, i, f * h);
-   }
+    public static Vec3 fromPolar(float pitch, float yaw) {
+        float f = MathHelper.cos(-yaw * 0.017453292F - 3.1415927F);
+        float g = MathHelper.sin(-yaw * 0.017453292F - 3.1415927F);
+        float h = -MathHelper.cos(-pitch * 0.017453292F);
+        float i = MathHelper.sin(-pitch * 0.017453292F);
+        return new Vec3(g * h, i, f * h);
+    }
 
-   public static float[] calculateYawPitch(Vec3 start, Vec3 vec) {
-       double diffX = vec.xCoord - start.xCoord;
-      double diffY = vec.yCoord - start.yCoord;
-      double diffZ = vec.zCoord - start.zCoord;
-      double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
-      float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0f;
-      float pitch = (float) (-Math.toDegrees(Math.atan2(diffY, diffXZ)));
-      return new float[]{MathHelper.wrapAngleTo180_float(yaw), MathHelper.wrapAngleTo180_float(pitch)};
-   }
+    public static float[] calculateYawPitch(Vec3 start, Vec3 vec) {
+        double diffX = vec.xCoord - start.xCoord;
+        double diffY = vec.yCoord - start.yCoord;
+        double diffZ = vec.zCoord - start.zCoord;
+        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+        float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0f;
+        float pitch = (float) (-Math.toDegrees(Math.atan2(diffY, diffXZ)));
+        return new float[]{MathHelper.wrapAngleTo180_float(yaw), MathHelper.wrapAngleTo180_float(pitch)};
+    }
 
-   public static Vec3 getMotionVec(Entity entity, float ticks, float yTicks) {
-      double dX = entity.posX - entity.prevPosX;
-      double dY = entity.posY - entity.prevPosY;
-      double dZ = entity.posZ - entity.prevPosZ;
-      double entityMotionPosX = 0;
-      double entityMotionPosY = 0;
-      double entityMotionPosZ = 0;
-       for (double i = 1; i <= ticks; i = i + 0.3) {
-          for (double i2 = 1; i2 <= yTicks; i2 = i2 + 0.3) {
-             if (!mc.theWorld.checkBlockCollision(entity.getEntityBoundingBox().offset(dX * i, dY * i2, dZ * i))) {
-                entityMotionPosX = dX * i;
-                entityMotionPosY = dY * i2;
-                entityMotionPosZ = dZ * i;
-             } else {
-                break;
-             }
-          }
-       }
-
-       return new Vec3(entityMotionPosX, entityMotionPosY, entityMotionPosZ);
-   }
+    public static Vec3 getMotionVec(Entity entity, float ticks, float yTicks) {
+        double dX = entity.posX - entity.prevPosX;
+        double dY = entity.posY - entity.prevPosY;
+        double dZ = entity.posZ - entity.prevPosZ;
+        double entityMotionPosX = 0;
+        double entityMotionPosY = 0;
+        double entityMotionPosZ = 0;
+        for (double i = 1; i <= ticks; i = i + 0.3) {
+            for (double i2 = 1; i2 <= yTicks; i2 = i2 + 0.3) {
+                if (!mc.theWorld.checkBlockCollision(entity.getEntityBoundingBox().offset(dX * i, dY * i2, dZ * i))) {
+                    entityMotionPosX = dX * i;
+                    entityMotionPosY = dY * i2;
+                    entityMotionPosZ = dZ * i;
+                } else {
+                    break;
+                }
+            }
+        }
+        return new Vec3(entityMotionPosX, entityMotionPosY, entityMotionPosZ);
+    }
 }
