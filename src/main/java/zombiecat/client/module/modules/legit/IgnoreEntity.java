47ffd96264055c5214fc99c90c7d81261f5046b7
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-06-28 21:23:20 +0100

    Update IgnoreEntity.java

--- src/main/java/zombiecat/client/module/modules/legit/IgnoreEntity.java
+++ src/main/java/zombiecat/client/module/modules/legit/IgnoreEntity.java
@@ -1,7 +1,20 @@
 package zombiecat.client.module.modules.legit;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityCaveSpider;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntityEndermite;
+import net.minecraft.entity.monster.EntityGhast;
+import net.minecraft.entity.monster.EntityGuardian;
+import net.minecraft.entity.monster.EntityMagmaCube;
+import net.minecraft.entity.monster.EntitySilverfish;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.monster.EntityWitch;
+import net.minecraft.entity.monster.EntityZombie;
 import zombiecat.client.module.Module;
 import zombiecat.client.module.setting.impl.BooleanSetting;
 
@@ -30,11 +43,24 @@ public class IgnoreEntity extends Module {
         if (!isOn || entity == null) return false;
 
         if (ignoreMobs.getValue()) {
-            // Ignore all entities if setting is enabled
+            // Ignore all entities
             return true;
-        } else {
-            // Ignore entities that are NOT mobs (players, animals, armor stands, etc)
-            return !(entity instanceof IMob);
         }
+
+        // List of allowed hostile mobs
+        return !(entity instanceof EntityZombie ||
+                 entity instanceof EntitySkeleton ||
+                 entity instanceof EntitySpider ||
+                 entity instanceof EntityCaveSpider ||
+                 entity instanceof EntityCreeper ||
+                 entity instanceof EntityEnderman ||
+                 entity instanceof EntityEndermite ||
+                 entity instanceof EntitySilverfish ||
+                 entity instanceof EntityWitch ||
+                 entity instanceof EntitySlime ||
+                 entity instanceof EntityMagmaCube ||
+                 entity instanceof EntityBlaze ||
+                 entity instanceof EntityGhast ||
+                 entity instanceof EntityGuardian);
     }
 }
