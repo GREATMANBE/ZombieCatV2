c765485ea1795e58f19277723f18616836e03d2b
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-07-10 12:09:13 +0100

    Update WindowDetect.java

--- src/main/java/zombiecat/client/module/modules/legit/WindowDetect.java
+++ src/main/java/zombiecat/client/module/modules/legit/WindowDetect.java
@@ -1,2 +1,10 @@
 package zombiecat.client.module.modules.legit;
 
+import zombiecat.client.module.Module;
+
+public class WindowDetect extends Module {
+    public WindowDetect() {
+        super("WindowDetect", ModuleCategory.legit);
+        // your constructor code
+    }
+}
