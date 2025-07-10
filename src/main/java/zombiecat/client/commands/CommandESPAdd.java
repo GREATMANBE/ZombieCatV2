5c324137b639a1e2f52798b7f79093405eef9187
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-07-10 11:33:51 +0100

    Update CommandESPAdd.java

--- src/main/java/zombiecat/client/commands/CommandESPAdd.java
+++ src/main/java/zombiecat/client/commands/CommandESPAdd.java
@@ -3,7 +3,7 @@ package zombiecat.client.commands;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.ChatComponentText;
 import zombiecat.client.module.modules.legit.ESP;
 
 public class CommandESPAdd extends CommandBase {
@@ -25,7 +25,7 @@ public class CommandESPAdd extends CommandBase {
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
+     public void execute(MinecraftServer server, ICommandSender sender, String[] args) {
         if (args.length == 4 && args[0].equalsIgnoreCase("add")) {
             try {
                 int x = Integer.parseInt(args[1]);
@@ -33,12 +33,12 @@ public class CommandESPAdd extends CommandBase {
                 int z = Integer.parseInt(args[3]);
 
                 espModule.addTrackedCoordinate(x, y, z);
-                sender.sendMessage(new TextComponentString("Added ESP tracked coordinate: " + x + ", " + y + ", " + z));
+                sender.addChatMessage(new ChatComponentText("Added ESP tracked coordinate: " + x + ", " + y + ", " + z));  // Changed here
             } catch (NumberFormatException e) {
-                sender.sendMessage(new TextComponentString("Invalid coordinates! Use integers."));
+                sender.addChatMessage(new ChatComponentText("Invalid coordinates! Use integers."));
             }
         } else {
-            sender.sendMessage(new TextComponentString("Usage: /esp add <x> <y> <z>"));
+            sender.addChatMessage(new ChatComponentText("Usage: /esp add <x> <y> <z>"));
         }
     }
 }
