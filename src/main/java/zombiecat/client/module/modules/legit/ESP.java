213f50a31433e341eab42f41a79ea8eac268f152
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-07-10 20:52:15 +0100

    Update ESP.java

--- src/main/java/zombiecat/client/module/modules/legit/ESP.java
+++ src/main/java/zombiecat/client/module/modules/legit/ESP.java
@@ -42,7 +42,7 @@ public class ESP extends Module {
                     && !(entity instanceof EntityCow)
                     && entity.isEntityAlive()) {
 
-                if (((EntityLivingBase) entity).isPotionActive(Potion.invisibility)) {
+               if (((EntityLivingBase) entity).isPotionActive(Potion.invisibility)) {
                   Utils.HUD.drawBoxAroundEntity(entity, true, Color.blue.getRGB());
                }
                if (entity instanceof EntityZombie && ((EntityZombie) entity).isChild() && entity.getInventory() != null && entity.getInventory()[0] != null && entity.getInventory()[0].getItem() == Items.diamond_sword) {
@@ -114,8 +114,6 @@ public class ESP extends Module {
                      Utils.HUD.drawBoxAroundEntity(entity, true, Color.red.getRGB());
                   } else if (chestGold && legsGold && bootsGold && holdingNothing) {
                      Utils.HUD.drawBoxAroundEntity(entity, true, Color.red.getRGB());
-                  } else if (entity instanceof EntityZombie && ((EntityLivingBase) entity).isPotionActive(Potion.invisibility)) {
-                     Utils.HUD.drawBoxAroundEntity(entity, true, Color.blue.getRGB());
                   } else {
                      Utils.HUD.drawBoxAroundEntity(entity, true, Color.green.getRGB());
                   }
@@ -145,9 +143,14 @@ public class ESP extends Module {
       Color color = new Color(255, 0, 0, 150);
       for (Entity entity : entities) {
          if (entity != thePlayer) {
-            if (entity instanceof EntityZombie && ((EntityZombie) entity).isChild() && entity.getInventory() != null && entity.getInventory()[0] != null && entity.getInventory()[0].getItem() == Items.diamond_sword) {
+            if (entity instanceof EntityZombie && ((EntityZombie) entity).isChild()
+                    && entity.getInventory() != null
+                    && entity.getInventory()[0] != null
+                    && entity.getInventory()[0].getItem() == Items.diamond_sword) {
                drawTraces(entity, color);
             }
+
+            if (entity instanceof EntityZombie) {
                EntityLivingBase living = (EntityLivingBase) entity;
                ItemStack chest = living.getEquipmentInSlot(3);
                ItemStack legs = living.getEquipmentInSlot(2);
@@ -223,7 +226,7 @@ public class ESP extends Module {
                }
             }
          }
-      
+      }
 
       GL11.glEnd();
 
@@ -233,7 +236,7 @@ public class ESP extends Module {
       GL11.glDepthMask(true);
       GL11.glDisable(GL11.GL_BLEND);
       GL11.glColor4f(1f, 1f, 1f, 1f);
-}
+   }
 
    private void drawTraces(Entity entity, Color color) {
       Entity thePlayer = mc.thePlayer;
@@ -255,6 +258,5 @@ public class ESP extends Module {
       GL11.glVertex3d(x, y, z);
       GL11.glVertex3d(x, y + entity.height, z);
    }
-}
 
-
+   }
